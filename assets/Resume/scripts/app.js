ba7f68a4681be130d33fd97a1ac47b942d0354e3
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2024-09-08 15:05:55 +0500

    Project Complete

---
+++ assets/Resume/scripts/app.js
@@ -0,0 +1,20 @@
+"use strict";
+$(document).ready(function () {
+    // Initialize the repeater plugin
+    $('.repeater').repeater({
+        initEmpty: false,
+        defaultValues: {
+            'text-input': ''
+        },
+        show: function () {
+            $(this).slideDown();
+        },
+        hide: function (deleteElement) {
+            $(this).slideUp(deleteElement);
+            setTimeout(() => {
+                generateCV();
+            }, 500);
+        },
+        isFirstItemUndeletable: true
+    });
+});
